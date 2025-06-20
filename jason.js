ea1dce7860fa3e31de23f37e27d61bea0169788f
HARINI18-sys <harinisaravanan1808@gmail.com>
2025-06-20 10:28:46 +0530

    complete json

---
+++ jason.js
@@ -0,0 +1,12 @@
+let jsObject = {
+  "name": "Harini",
+  "age": 19
+};
+
+let stringJSON = JSON.stringify(jsObject); // Convert object to JSON string
+console.log(stringJSON);                   // Output: {"name":"Harini","age":19}
+console.log(typeof stringJSON);            // Output: string
+
+let normalJSON = JSON.parse(stringJSON);   // Convert JSON string back to object
+console.log(normalJSON);                   // Output: { name: 'Harini', age: 19 }
+console.log(typeof normalJSON);            // Output: object
