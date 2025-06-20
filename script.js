e5b13f18897e814d7a602f7cf521e969337153f7
HARINI18-sys <harinisaravanan1808@gmail.com>
2025-06-20 10:11:03 +0530

    complete console

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Normal log");
+console.error("Error message");
+console.warn("Warning message");
+
+console.table([
+  { name: "Harini", age: 19 },
+  { name: "Varna", age: 21 }
+]);
+
+console.time("calc");
+let sum = 0;
+for (let i = 0; i < 1e6; i++) sum += i;
+console.timeEnd("calc");
